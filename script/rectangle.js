622ce38dc8cb1379a33f7a7266752243dad28a11
tirthoraj Mondal <tirtho.raj.2003@gmail.com>
2024-02-15 02:22:31 +0600

    first commit

---
+++ script/rectangle.js
@@ -0,0 +1,18 @@
+function rectangleAreaCalculate() {
+    const rectrangleLengthInput = document.getElementById('rectangle-length');
+    const rectrangleLengthText = rectrangleLengthInput.value;
+    const length = parseFloat(rectrangleLengthText);
+    console.log(length);
+    
+    
+    const rectrangleWidthInput = document.getElementById('rectangle-width');
+    const rectrangleWidthText = rectrangleWidthInput.value;
+    const width = parseFloat(rectrangleWidthText);
+    console.log(width);
+
+    const area = length*width;
+    console.log('Area of the rectangle is:' , area);
+
+    const rectangleAreaSpan = document.getElementById('rectangle-area');
+    rectangleAreaSpan.innerText = area;
+}
---
+++ script/tringle.js
@@ -0,0 +1,18 @@
+function calculateTriangleArea() {
+    const triangleBaseInput = document.getElementById('triangle-base');
+    const triangleBaseText = triangleBaseInput.value;
+    const base = parseFloat(triangleBaseText);
+    console.log(base);
+
+    const triangleHeightInput = document.getElementById('triangle-height');
+    const triangleHeightText = triangleHeightInput.value;
+    const height = parseFloat(triangleHeightText);
+    console.log(height);
+
+    const area = 0.5 * base * height;
+    console.log('Area of traingle is:' ,area);
+
+    const triangleAreaSpan = document.getElementById('traingle-area');
+    triangleAreaSpan.innerText = area;
+
+}
